b3e7f59e226df3bbc97e3a54f38e732162732e88
erick <53189159+erick-GeGe@users.noreply.github.com>
2021-12-30 20:22:57 -0500

    commenting code

--- quadtree.h
+++ quadtree.h
@@ -5,30 +5,31 @@
 class Rectangle
 {
 public:
+    // Rango del rectangulo
     int a, b, c, d;
     cv::Vec3b average;
-    bool isleaf;
+    
 
     Rectangle()
     {
         a = b = c = d = 0;
-        isleaf = false;
     }
 
     Rectangle(int _a, int _b, int _c, int _d)
     {
-        isleaf = false;
         a = _a;
         b = _b;
         c = _c;
         d = _d;
     }
 
+    // Se calcula el color promedio de la imagen
     void calculateAverage(cv::Mat img)
     {
         int count = 0;
         unsigned int cr, cg, cb;
         cr = cg = cb = 0;
+        // Sumando todos los colores del rectangulo
         for (int i = a; i <= b; i++)
         {
             for (int j = c; j <= d; j++)
@@ -40,28 +41,18 @@ public:
                 cb += color.val[2];
             }
         }
+        // Obteniendo promedio
         average.val[0] = cr / count;
         average.val[1] = cg / count;
         average.val[2] = cb / count;
     }
 
+    // Se le pasa un color para asignarlo a todo un rectangulo
     void changeColor(cv::Vec3b color, cv::Mat img, Rectangle *boundary_b)
     {
         for (int i = a; i < b; i++)
-        {
             for (int j = c; j < d; j++)
-            {
-                // if( j - boundary_b->c > img.rows)
-                //     continue;
-                // if( i - boundary_b->a > img.cols)
-                //     continue;
-                // std::cout << img.cols << " " << img.rows << std::endl;
-                // std::cout << c << " " << d << std::endl;
-                // std::cout << i - boundary_b->a << " " << c << std::endl;
                 img.at<cv::Vec3b>(cv::Point(i - boundary_b->a, j - boundary_b->c)) = color;
-                // img.at<cv::Vec3b>(cv::Point(i, j)) = color;
-            }
-        }
     }
 };
 
@@ -73,18 +64,19 @@ class QuadTree
     Rectangle *boundary;
     QuadTree *childrens[4];
     Rectangle *childrensb[4];
-    std::string filecompress;
 
+    // Funcion que se encarga de comprimir la imagen
     void compress(Rectangle *boundary_b)
     {
         int c1, c2;
+        // Se obtiene las mitades de la imagen
         c1 = (boundary->b - boundary->a) / 2;
         c2 = (boundary->d - boundary->c) / 2;
         subdivide(c1, c2);
-        // std::cout << "alo?\n";
 
         if (partible() && c1 && c2)
         {
+            // Ya que debe partirse, se crean los 4 nuevos hijos
             for (size_t i = 0; i < 4; i++)
             {
                 childrens[i] = new QuadTree(childrensb[i], img, threshold, new_img);
@@ -93,27 +85,16 @@ class QuadTree
         }
         else
         {
-            // cv::Vec3b averages;
-            // for(int i = 0; i < 4; i++)
-            //     for (size_t j = 0; j < 3; j++)
-            //         averages.val[j] += (childrensb[i]->average.val[j])/4;
-
-            // boundary->average = averages;
-            // boundary->changeColor(boundary->average, img);
-            // std::cout << "fue aqui\n";
-
+            // Ya que no ser parte se asigna el color promedio de la imagen al rectangulo
             for (size_t i = 0; i < 4; i++)
-            {
                 childrensb[i]->changeColor(childrensb[i]->average, new_img, boundary_b);
-            }
-            // std::cout << "fue aqui2\n";
-            // boundary->isleaf = true;
         }
     }
 
+    // Se verifica si el rectangulo debe partirse
     bool partible()
     {
-
+        // Se obtiene el promedio de colores
         int averages[3]{0};
         for (size_t i = 0; i < 4; i++)
         {
@@ -122,18 +103,21 @@ class QuadTree
                 averages[j] += (childrensb[i]->average.val[j]) / 4;
         }
 
+        // Se calcula la diferencia entre el los colores promedio y el promedio de promedios
         for (size_t i = 0; i < 3; i++)
         {
             int dif = 0;
             for (size_t j = 0; j < 4; j++)
                 dif += abs(childrensb[j]->average.val[i] - averages[i]);
-
+            
+            // Se verifica que no supere el threshold
             if (dif > threshold)
                 return true;
         }
         return false;
     }
 
+    // El rectangulo se divide en 4 rectangulos mas pequeños
     void subdivide(int c1, int c2)
     {
         childrensb[0] = new Rectangle(boundary->a, boundary->a + c1, boundary->c, boundary->c + c2);
@@ -146,7 +130,6 @@ public:
     QuadTree(Rectangle *b, cv::Mat image, int t, cv::Mat n_image)
     {
         new_img = n_image;
-        filecompress = "";
         img = image;
         threshold = t;
         boundary = b;
@@ -162,7 +145,8 @@ public:
         if (boundary)
             delete boundary;
     }
-
+    
+    // Funcion publica para comprimir imagen
     void compress_image()
     {
 
